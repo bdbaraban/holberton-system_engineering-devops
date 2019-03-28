d94bdaaf569a76b22022b1f643d76d08607ca13e
bdbaraban <375@holbertonschool.com>
2019-03-28 06:54:02 +0000

    Touch tar file before zipping to prevent changing directory warning

--- 0x0B-web_server/fabfile.py
+++ 0x0B-web_server/fabfile.py
@@ -7,6 +7,7 @@ from fabric import task
 @task
 def pack(c):
     """Create a tar gzipped archive of the current directory."""
+    c.run("touch holbertonwebapp.tar.gz")
     c.run("tar --exclude='*.tar.gz' -cvzf holbertonwebapp.tar.gz .")
 
 
