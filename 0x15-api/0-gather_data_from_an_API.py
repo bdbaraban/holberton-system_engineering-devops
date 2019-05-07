38ff053ff590f7740dd3e13679d2607b67d9cc71
bdbaraban <375@holbertonschool.com>
2019-05-07 00:14:53 +0000

    Print tabulation before space for completed tasks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    [print(" \t{}".format(c)) for c in completed]
+    [print("\t {}".format(c)) for c in completed]
