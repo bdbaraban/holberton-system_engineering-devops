f5f571d410959aafb4014ba57a3129c5c5fad330
bdbaraban <375@holbertonschool.com>
2019-05-09 20:22:58 +0000

    Add return statement after printing None in cases where subreddit is not matched

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,5 +16,6 @@ def top_ten(subreddit):
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
+        return
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
