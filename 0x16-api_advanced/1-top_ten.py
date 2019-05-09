22d4b2dddecce9526c2a202c56c8f2f98d020a65
bdbaraban <375@holbertonschool.com>
2019-05-09 20:12:23 +0000

    Upload task 1: function to print hot posts on a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/top/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
