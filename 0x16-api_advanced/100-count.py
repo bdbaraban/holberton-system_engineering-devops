38814ec62e73317dcf357e15ab83c692699b4bcd
bdbaraban <375@holbertonschool.com>
2019-05-09 22:31:01 +0000

    Error fix - was looping over word_list instead of title

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -35,7 +35,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         title = c.get("data").get("title").lower().split(" ")
         for word in word_list:
             if word.lower() in title:
-                times = len([w for w in word_list if w == word.lower()])
+                times = len([t for t in title if t == word.lower()])
                 if instances.get(word) is None:
                     instances[word] = times
                 else:
