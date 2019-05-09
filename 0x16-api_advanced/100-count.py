b9a3c08bd851df327eeeb6c51387f289a3404514
bdbaraban <375@holbertonschool.com>
2019-05-09 22:06:34 +0000

    Upload bonus task 3: Script that queries word counts in post titles for a given subreddit

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Function to count words in all hot posts of a given Reddit subreddit."""
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Prints counts of given words found in hot posts of a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count
+        "limit": 100,
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split(" ")
+        for word in word_list:
+            if word.lower() in title:
+                if instances.get(word) is None:
+                    instances[word] = 1
+                else:
+                    instances[word] += 1
+
+    if after is None:
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
