6a2844a3c5e5e47fb0e605169be13be252db3c43
bdbaraban <375@holbertonschool.com>
2019-05-09 19:50:58 +0000

    Upload task 0: function that queries subscribers on subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    page = requests.get(url, headers=headers, allow_redirects=False)
+    if page.status_code == 404:
+        return 0
+    return page.json().get("data").get("subscribers")
